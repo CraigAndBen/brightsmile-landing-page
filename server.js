17703481f88da9b9f9e666381dfddf7087b82114
Craig Ben Cadag <craigbencadag@gmail.com>
2026-05-03 15:31:31 +0800

    add cors method

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 5000;
 dotenv.config({ path: '.env.local' });
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: "https://brightsmile-dental.craigben.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
